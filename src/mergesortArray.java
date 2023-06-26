6685a2859299ad34a2834505688a95133918cc6c
namsi-lia <namsilydia7@gmail.com>
2023-06-26 17:00:45 +0300

    Explanation and approach

--- src/mergesortArray.java
+++ src/mergesortArray.java
@@ -11,6 +11,33 @@
  *         and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
  */
 
+/**
+ * Approach to solve this
+ * Traverse nums2 array from the end traverse nums2 array as it is given that nums1 array will always be greater than equal to nums2 array and we will traverse nums2 array from end as the array and we will traverse nums2 array from end as the
+ * array is sorted ans we need largest eleent to solve the problem
+ * compare the largest element of the nums2 array with the largest of the nums1 array m-1 index
+ * the largest element after comparing both the arrays will be stores at the end of nums1 array at nums1.length-1 index
+ *
+ *
+ */
+/**
+ * How to solve this
+ * 1. Declare 3 variables i,j,k which will act as pointers
+ * variable i will point at m-1(index),which holds the largest element of the nums1 array
+ * variable j will point at n-1(index) thats the last position of the nums2 array
+ * which holds the largest element of the nums 2 array.
+ * while k will point to the last position of the nums1 array and will insert the largest element after comparing both arrays
+ *
+ * 2.We will traverse the nums2 array and will check the 2 conditions :
+ * 1 : If index i is greater than 0 and also the element at i index of nums1 array is greater than the element at j index of nums2 array.
+ * If this condition is true, it means element at i index of nums1 array is largest among both the arrays and will be stored at last index/position of the nums1 array.
+ * After this we will decrease the value of i as we have to compare the other element of nums1 array with element of nums2 array.
+ * We will also decrease the value of k as the last position is filled and now new position is needed.
+ * 2 : If condition 1 fails, it means that either the element at j index of nums2 array is greater than the element at i index of nums1 array or there is no element left to compare with the nums1 array and we just have to push the elements of nums2 array in the nums1 array (They are the remaining smallest elements).
+ * We will decrease the value of j for other comparisons and also the value of k to store other elements.
+ */
+
+
 class Solution{
     public static void merge(int[] nums1,int m, int[] nums2, int n){
         int i =m-1;
